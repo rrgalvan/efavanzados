3124be2fb1338d37f57bdbc06b69dae08872b3a2
Rafa Rodríguez Galván <rafael.rodriguez@uca.es>
2021-03-10 12:32:03 +0100

    Añado MFEM-first-test.cpp CMakeLists.txt

--- contenidos.md
+++ contenidos.md
@@ -1,13 +1,17 @@
 # Contenidos
 
+## Semana 1
 1. Introducción a la "shell" en entornos Unix. Sistemas de control de versiones. Lenguaje de programación Python. Introducción al entorno de elementos finitos FEniCS.
 2. El lenguaje de programación C++. Programación orientada a objetos. Implementación de bibliotecas de elementos finitos.
+## Semana 2
 3. La biblioteca de elementos finitos C++ MFEM. Problemas de evolución. Sistemas de EDP.
 4. Elementos finitos de orden alto. Bases de Lobatto. Bases de Bernstein. Teoría e implementación en el ordenador.
 5. Elementos finitos de Hermite y elementos de clase C^1. Teoría e implementación en el ordenador.
+## Semana 3
 6. Elementos discontinuos para EDP elípticas y parabólicas. Análisis e implementación en el ordenador.
 7. Elementos discontinuos para EDP hiperbólicas. Análisis e implementación en el ordenador.
 8. Otras técnicas para resolución numérica de EDP hiperbólicas: GLS, corrección de flujo. Teoría e implementación en el ordenador.
+## Semana 4
 9. Implementación en grandes ordenadores de métodos numéricos paralelos en espacio y tiempo.
 10. Resolución numérica de EDP para el bilaplaciano y EDP de orden cuatro. Teoría e implementación en el ordenador.
 11. Refinamiento de malla y h/p-adaptabilidad. Teoría e implementación en el ordenador.
---
+++ src/mfem/MFEM-first-test.cpp
@@ -0,0 +1,136 @@
+
+#include "mfem.hpp"
+#include <fstream>
+#include <iostream>
+
+using namespace std;
+using namespace mfem;
+
+int main(int argc, char *argv[])
+{
+   // 1. Parse command-line options.
+   const char *mesh_file = "../data/star.mesh";
+   int order = 1;
+   const char *device_config = "cpu";
+   bool visualization = true;
+
+   OptionsParser args(argc, argv);
+   args.AddOption(&mesh_file, "-m", "--mesh",
+                  "Mesh file to use.");
+   args.AddOption(&order, "-o", "--order",
+                  "Finite element order (polynomial degree) or -1 for"
+                  " isoparametric space.");
+   args.AddOption(&device_config, "-d", "--device",
+                  "Device configuration string, see Device::Configure().");
+   args.AddOption(&visualization, "-vis", "--visualization", "-no-vis",
+                  "--no-visualization",
+                  "Enable or disable GLVis visualization.");
+   args.Parse();
+   if (!args.Good())
+   {
+      args.PrintUsage(cout);
+      return 1;
+   }
+   args.PrintOptions(cout);
+
+   // 2. Enable hardware devices such as GPUs, and programming models such as
+   //    CUDA, OCCA, RAJA and OpenMP based on command line options.
+   Device device(device_config);
+   device.Print();
+
+   // 3. Read the mesh from the given mesh file. We can handle triangular,
+   //    quadrilateral, tetrahedral, hexahedral, surface and volume meshes with
+   //    the same code.
+   Mesh mesh(mesh_file, 1, 1);
+   int dim = mesh.Dimension();
+
+   // 4. Refine the mesh to increase the resolution.
+   {
+     int ref_levels = 4; // 2^(4n) elementos, n = ref_levels
+     for (int l = 0; l < ref_levels; l++)
+       {
+         mesh.UniformRefinement();
+       }
+   }
+
+   // 5. Define a finite element space on the mesh. Here we use continuous
+   //    Lagrange finite elements of the specified order. If order < 1, we
+   //    instead use an isoparametric/isogeometric space.
+   // FiniteElementCollection *fec;
+   // assert(order>0);
+   H1_FECollection fec(order, dim);
+   FiniteElementSpace fespace(&mesh, &fec);
+   cout << "Number of finite element unknowns: "
+        << fespace.GetTrueVSize() << endl;
+
+   // 6. Determine the list of true (i.e. conforming) essential boundary dofs.
+   //    In this example, the boundary conditions are defined by marking all
+   //    the boundary attributes from the mesh as essential (Dirichlet) and
+   //    converting them to a list of true dofs.
+   Array<int> ess_tdof_list;
+   if (mesh.bdr_attributes.Size())
+   {
+      Array<int> ess_bdr(mesh.bdr_attributes.Max());
+      ess_bdr = 1;
+      fespace.GetEssentialTrueDofs(ess_bdr, ess_tdof_list);
+   }
+
+   // 7. Set up the linear form b(.) which corresponds to the right-hand side of
+   //    the FEM linear system, which in this case is (1,phi_i) where phi_i are
+   //    the basis functions in the finite element fespace.
+   LinearForm b(&fespace);
+   ConstantCoefficient one(1.0);
+   b.AddDomainIntegrator(new DomainLFIntegrator(one));
+   b.Assemble();
+
+   // 8. Define the solution vector x as a finite element grid function
+   //    corresponding to fespace. Initialize x with initial guess of zero,
+   //    which satisfies the boundary conditions.
+   GridFunction x(&fespace);
+   x = 0.0;
+
+   // 9. Set up the bilinear form a(.,.) on the finite element space
+   //    corresponding to the Laplacian operator -Delta, by adding the Diffusion
+   //    domain integrator.
+   BilinearForm a(&fespace);
+   a.AddDomainIntegrator(new DiffusionIntegrator(one));
+
+   // 10. Assemble the bilinear form and the corresponding linear system,
+   //     applying any necessary transformations such as: eliminating boundary
+   //     conditions, applying conforming constraints for non-conforming AMR,
+   //     static condensation, etc.
+   a.Assemble();
+
+   OperatorPtr A;
+   Vector B, X;
+   a.FormLinearSystem(ess_tdof_list, x, b, A, X, B);
+
+   cout << "Size of linear system: " << A->Height() << endl;
+
+   // 11. Solve the linear system A X = B.
+   // If MFEM was compiled with SuiteSparse, use UMFPACK to solve the system.
+   UMFPackSolver umf_solver;
+   umf_solver.Control[UMFPACK_ORDERING] = UMFPACK_ORDERING_METIS;
+   umf_solver.SetOperator(*A);
+   umf_solver.Mult(B, X); // X=A^-1*B
+
+   // 12. Recover the solution as a finite element grid function.
+   a.RecoverFEMSolution(X, b, x);
+
+   // 13. Save the refined mesh and the solution. This output can be viewed later
+   //     using GLVis: "glvis -m refined.mesh -g sol.gf".
+   ofstream mesh_ofs("refined.mesh");
+   mesh_ofs.precision(8);
+   mesh.Print(mesh_ofs);
+   ofstream sol_ofs("sol.gf");
+   sol_ofs.precision(8);
+   x.Save(sol_ofs);
+
+   // // 15. Free the used memory.
+   // if (delete_fec)
+   // {
+   //    delete fec;
+   // }
+
+   return 0;
+}
